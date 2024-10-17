414c13fda89cda91d58c0e786f5c2f4c93fdf761
ericl339 <148896243+ericl339@users.noreply.github.com>
2024-10-17 16:20:12 +0000

    project

---
+++ src/main/java/com/example/project/LinearCalculatorRunner.java
@@ -0,0 +1,7 @@
+package com.example.project;
+public class LinearCalculatorRunner {
+    public static void main(String[] args) {
+        LinearCalculator test1 = new LinearCalculator("(-5,1)","(1,1)");
+        System.out.println(test1.printInfo());
+    }
+}
